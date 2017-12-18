50615fda61dd6df5163400a5e8c59e2f45875590
Auston Stewart <auston@shedbuilt.net>
2017-12-17 17:47:07 -1000

    Updating packaging and fixing build script for non-bootstrap

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# Patch an issue with glibc-2.26
 sed -i "/math.h/a #include <malloc.h>" src/flexdef.h
-HELP2MAN=/tools/bin/true \
+HELP2MAN=/bin/true \
 ./configure --prefix=/usr \
-            --docdir=/usr/share/doc/flex-2.6.4
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+            --docdir=/usr/share/doc/flex-2.6.4 || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make "DESTDIR=${SHED_FAKEROOT}" install || exit 1
 ln -sv flex ${SHED_FAKEROOT}/usr/bin/lex
