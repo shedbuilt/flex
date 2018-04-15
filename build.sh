eb730637dcad6c54aa602ef6a80d97fb6a7a2512
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:23:25 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ sed -i "/math.h/a #include <malloc.h>" src/flexdef.h
 HELP2MAN=/bin/true \
 ./configure --prefix=/usr \
             --docdir=/usr/share/doc/flex-2.6.4 || exit 1
-make -j $SHED_NUMJOBS || exit 1
-make "DESTDIR=${SHED_FAKEROOT}" install || exit 1
-ln -sv flex ${SHED_FAKEROOT}/usr/bin/lex
+make -j $SHED_NUM_JOBS || exit 1
+make "DESTDIR=${SHED_FAKE_ROOT}" install || exit 1
+ln -sv flex ${SHED_FAKE_ROOT}/usr/bin/lex
